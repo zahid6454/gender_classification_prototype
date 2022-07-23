889eaca88afdd1dde7e261dcec2dea30ffbc3b07
Zahid Hossain <zahid.hossain@reddotdigitalit.com>
2022-07-23 16:35:09 +0600

    Heroku - Preparing for Hosting

--- engine/engine/settings.py
+++ engine/engine/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^u%4%ud*23a%k6ntmx6xdmzvkg4=i&f=5knsiv^chwo_1wc(oh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','https://gender--classification.herokuapp.com/']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
